84bb0f28ad9a48b4a31b5bcda31362440f0677c6
jsebastianchavarro00-hash <jsebastianchavarro00@gmail.com>
2026-06-30 18:42:45 -0500

    Update index.js

--- Tag-airlines-bot/index.js
+++ Tag-airlines-bot/index.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
+console.log("TOKEN existe:", !!process.env.TOKEN);
+console.log("Longitud del token:", process.env.TOKEN ? process.env.TOKEN.length : 0);
+
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds],
 });
 
 client.once("ready", () => {
